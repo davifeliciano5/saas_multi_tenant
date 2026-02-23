68bfa363c38ed5fb65fc1b5f261b64edb13504fe
davifeliciano5 <davifelicianosousa2020@gmail.com>
2026-02-23 10:28:49 -0300

    default

---
+++ agendamento/src/test/java/com/davi/saas/agendamento/AgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.saas.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
